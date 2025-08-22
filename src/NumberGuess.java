f790246baee22165e3ab8965657463e9a0d18513
Bongani X <velanibzuma@gmail.com>
2025-08-23 00:11:52 +0200

    setting up variables

--- src/NumberGuess.java
+++ src/NumberGuess.java
@@ -1,7 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class NumberGuess{
 
     public static void main(String [] args){
-        
+
+        int generatedNumber;
+        int userGuess;
+        int attemptsLeft;
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();        
+        System.out.print("Guess the number (1-100)");
+
+        try{
+            generatedNumber = random.nextInt(1,101);
+            userGuess = scanner.nextInt();
+        }catch(Exception e){
+
+        }
+
     }
 
 }
