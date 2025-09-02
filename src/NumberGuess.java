fad3dc700fc1eca991ca5a595a15fb8ed1494feb
Bongani X <velanibzuma@gmail.com>
2025-09-02 18:24:29 +0200

    -logic refined -used nextLine() as primary capture to avoid newline errors -added finally block to close scanner resource

--- src/NumberGuess.java
+++ src/NumberGuess.java
@@ -12,14 +12,23 @@ public class NumberGuess{
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         System.out.println("****************NUMBER GUESSING GAME****************");        
-        System.out.print("Guess the number (1-100): ");
+        
 
         generatedNumber = random.nextInt(100) + 1;
 
         try{
             
             do{
-                userGuess = scanner.nextInt();
+                System.out.printf("Guess the number, you have %d chances remaining (1-100) : ", attemptsLeft);
+
+                String input = scanner.nextLine();
+
+                if(input.isEmpty()){
+                    System.out.println("Enter a NUMBER!!");
+                    continue;
+                }
+
+                userGuess = Integer.parseInt(input);
                 
 
                 
@@ -27,67 +36,56 @@ public class NumberGuess{
                     System.out.printf("WOW! YOU GOT IT! \nThe number was %d",generatedNumber);
                     break;
                 }
-                if(userGuess > 100){
-                    System.out.print("The RANGE is 1-100!! :");
-                    attemptsLeft--;
-                    scanner.nextLine();
-                    continue;
-                }
-                 if(userGuess < 0 || userGuess == 0){
-                    System.err.print("The number is surely greater than 0!! :");
-                    attemptsLeft--;
-                    scanner.nextLine();
-                }
-                else if(userGuess > generatedNumber && (userGuess - generatedNumber > 18)){
-                    System.out.printf("The number %d is too high :",userGuess);
-                    attemptsLeft--;
-                    scanner.nextLine();
-                }
-                else if(userGuess < generatedNumber && (generatedNumber - userGuess > 18)){
-                    System.out.printf("The number %d is too low. TRY AGAIN: ",userGuess);
-                    attemptsLeft--;
-                    scanner.nextLine();
-                    continue;
-                }
-                else if(userGuess > generatedNumber && ((userGuess - generatedNumber < 12)&& (userGuess - generatedNumber > 6))){
-                    System.out.print("Not quite! but close, try again! :");
-                    attemptsLeft--;
-                    scanner.nextLine();
-                    continue;
-                }
-                else if(userGuess < generatedNumber && ((generatedNumber - userGuess < 12)&& (generatedNumber - userGuess > 6) )){
-                    System.out.print("Not quite! ! :");
-                    attemptsLeft--;
-                    scanner.nextLine();
-                }
-                else if(userGuess > generatedNumber && (((userGuess - generatedNumber < 7)))){
-                    System.out.print("REALLY CLOSE!! Keep at it!: ");
-                    attemptsLeft--;
-                    scanner.nextLine();
-                }
-                else if(userGuess < generatedNumber && ((generatedNumber - userGuess < 7) )){
-                    System.out.print("Super Close!!! :");
-                    attemptsLeft--;
-                    scanner.nextLine();
-                }
-
-                if( attemptsLeft < 6 && attemptsLeft >=1 ){
-                    System.out.printf("\nYou have %d guess(es) REMAINING!! :",attemptsLeft);
-                    
-                    attemptsLeft--;
-                    scanner.nextLine();
-                    //continue;
-                }
+               
+                attemptsLeft--;
 
                 if(attemptsLeft == 0){
-                    System.out.println("\nGAME OVER!!! \nThe NUMBER was: " + generatedNumber);
+                    break;
+                }
+
+                if(userGuess > 100){
+                    System.out.println("The RANGE is 1-100!! :");
+                    attemptsLeft--;
                     continue;
+                }
+                else if(userGuess <= 0){
+                    System.err.println("The number is surely greater than 0!! :");
+
+                }
+                else if(userGuess > generatedNumber && (userGuess - generatedNumber > 18)){
+                    System.out.printf("The number %d is too high : \n",userGuess);
+                }
+                else if(userGuess < generatedNumber && (generatedNumber - userGuess > 18)){
+                    System.out.printf("The number %d is too low. TRY AGAIN: \n",userGuess);
+
+                }
+                else if(userGuess > generatedNumber && ((userGuess - generatedNumber < 12)&& (userGuess - generatedNumber > 6))){
+                    System.out.println("Not quite! but close, try again! :");
+                    
+                }
+                else if(userGuess < generatedNumber && ((generatedNumber - userGuess < 12)&& (generatedNumber - userGuess > 6) )){
+                    System.out.println("Not quite! ! :");
+                    
+                }
+                else if(userGuess > generatedNumber && (((userGuess - generatedNumber < 7)))){
+                    System.out.println("REALLY CLOSE!! Keep at it!: ");
+                    
+                }
+                else if(userGuess < generatedNumber && ((generatedNumber - userGuess < 7) )){
+                    System.out.println("Super Close!!! :");
+                    
                 }
                
             }while(attemptsLeft>0);
 
-        }catch(Exception e){
+            if(attemptsLeft == 0 && userGuess != generatedNumber){
+                System.out.println("\nGAME OVER!!! \nThe NUMBER was: " + generatedNumber);
+            }
+
+        }catch(NumberFormatException e){
             System.err.println("Error");
+        }finally{
+            scanner.close();
         }
 
     }
